6b4c92b151b96208bcd3a96046fc12b0d673581e
ColdfishO <simon.jan.dudek@gmail.com>
2023-07-03 13:52:34 +0200

    Add project files.

---
+++ zad3.2/zad3.2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include<locale.h>
+#include "klasa.h"
+using namespace std;
+void autor(void) {
+    cout << endl;
+    for (int i = 1; i <= 120; i++) cout << "*";
+    cout << "Autor programu: Simon Dudek\n\n";
+    for (int i = 1; i <= 120; i++) cout << "*";
+    cout << "\n\n";
+}
+int main() {
+    double x, y;
+    setlocale(LC_ALL, "");
+    autor();
+    cout << "Wpisz pierwsza liczbę zepoloną(część rzeczywista): ";
+    cin >> x;
+    cout << "(Część urojona): ";
+    cin >> y;
+    Tzespol pierwsza(x, y);
+    cout << "Wpisz drugą liczbę zepoloną(część rzeczywista): ";
+    cin >> x;
+    cout << "(Część urojona): ";
+    cin >> y;
+    Tzespol druga(x, y);
+    Tzespol wynik;
+    wynik = pierwsza + druga;
+    cout << "Wynik dodawania: ";
+    wynik.wyświetl();
+    wynik = pierwsza - druga;
+    cout << "Wynik odejmowania: ";
+    wynik.wyświetl();
+    wynik = pierwsza * druga;
+    cout << "Wynik mnożenia: ";
+    wynik.wyświetl();
+    wynik = pierwsza / druga;
+    cout << "Wynik dzielenia: ";
+    wynik.wyświetl();
+    system("pause");
+    return 0;
+}
